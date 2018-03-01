655da5a94291b662b047a27edf2c1126b3b809e0
fahmed3 <fahmed3@stuy.edu>
2018-02-28 23:20:49 -0500

    finished

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 var s = document.getElementById("svg_id");
 var clearButton = document.getElementById("clr");
-var newLineButton = document.getElementById("new");
+var x1 = -1;
+var y1 = -1;
 
 var draw = function(e) {
-    var x1 = 0;
-    var y1 = 0;
     var x = e.offsetX;
     var y = e.offsetY;
     var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     circle.setAttribute("cx", x);
     circle.setAttribute("cy", y);
     circle.setAttribute("r", 15);
-    circle.setAttribute("fill", "blue");
+    circle.setAttribute("fill", "red");
+    circle.setAttribute("stroke", "black");
     s.appendChild(circle);
     var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    line.setAttribute("x1",x1);
-    line.setAttribute("y1",y1);
-    line.setAttribute("x2",x);
-    line.setAttribute("y2",y);
-    line.setAttribute("stroke","black");
-    s.appendChild(line);
+    if (x1 != -1 && y1 != -1){
+	line.setAttribute("x1",x1);
+	line.setAttribute("y1",y1);
+	line.setAttribute("x2",x);
+	line.setAttribute("y2",y);
+	line.setAttribute("stroke","black");
+	s.appendChild(line);
+    }
+    x1 = x;
+    y1 = y;
 }
 
-var clear = function(){
-    ctx.beginPath();
-    ctx.clearRect(0,0,c.width,c.height);
-}
-
-var newPath = function(){
-    ctx.beginPath();
+var clear = function() {
+    for(i = s.children.length-1; i >= 0; i--){
+	s.removeChild(s.children[i]);
+    }
+    x1 = -1;
+    y1 = -1;
 }
 
 s.addEventListener("click", draw);
 clearButton.addEventListener("click", clear);
-newLineButton.addEventListener("click", newPath);
 
